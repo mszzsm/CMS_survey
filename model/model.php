06fc82dd9e52594cb8d1d9e5e41158fdf9cf32d2
msz <mikeshtefanitsa@gmail.com>
2019-09-30 00:54:00 +0300

    Update model.php

--- model/model.php
+++ model/model.php
@@ -6,10 +6,10 @@ abstract class Model{
 
     protected function __construct(){
         try{
-            $hostname = "172.20.1.33";
+            $hostname = "127.0.0.1";
             $dbname = "survey";
             $username = "survey";
-            $pw = 'QWUuNRZxY5rkwNmo';
+            $pw = 'password';
             $this->pdo = new PDO('mysql:host='.$hostname.';dbname='.$dbname.';charset=utf8', $username, $pw);
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->pdo->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, false);  
